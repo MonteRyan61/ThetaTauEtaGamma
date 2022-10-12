e5f8751290e885f3544fd431809d72d332684314
MonteRyan61 <ryan.monteleone@colorado.edu>
2022-10-11 20:42:33 -0600

    Added some

--- main.js
+++ main.js
@@ -1,44 +1,72 @@
 
 //Correct sizing for the slideshow landscape photos
 let slideLandscape = document.querySelectorAll(".slideShowLandscape");
+let slideVertical = document.getElementById("slideVertical");
 function setHeightForLandscapeSlide() {
     let w = window.innerWidth;
     for (const flexItem of slideLandscape) {
         flexItem.style.height = w/4 + "px";
     }
+    // slideVertical.style.height = w/1.01 + "px";
     setTimeout("setHeightForLandscapeSlide()", 340);
 }
 document.addEventListener('DOMContentLoaded', setHeightForLandscapeSlide());
 
 let i = 0;
 let interval = 2000;
-let image = [];
+let horizontalImage = [];
 
 
 //Create an array of the images that will cycle through on the main page
-image[0] = "slideShow/landscape/gardenOfTheGods.JPG"
-image[1] = "slideShow/landscape/nationals.png"
-image[2] = "slideShow/landscape/pikespeak.png"
-image[3] = "slideShow/landscape/graduation2022.png"
-image[4] = "slideShow/landscape/campfire2022.png"
-image[5] = "slideShow/landscape/pledgeparty.png"
-image[6] = "slideShow/landscape/pledgeparty_1.png"
+horizontalImage[0] = "slideShow/landscape/gardenOfTheGods.JPG";
+horizontalImage[1] = "slideShow/landscape/nationals.png";
+horizontalImage[2] = "slideShow/landscape/pikespeak.png";
+horizontalImage[3] = "slideShow/landscape/graduation2022.png";
+horizontalImage[4] = "slideShow/landscape/campfire2022.png";
+horizontalImage[5] = "slideShow/landscape/pledgeparty.png";
+horizontalImage[6] = "slideShow/landscape/pledgeparty_1.png";
 
 
-let imageChange = document.getElementById("slide");
-function slideShow(){
-    console.log(image[i]);
-    imageChange.src = image[i];
-    if(i < image.length - 1)
+let horizontalImageChange = document.getElementById("slide");
+function horizontalSlideShow(){
+    if(i < horizontalImage.length - 1)
     {
-        i++
+        i++;
     }
     else
     {
         i = 0;
     }
-    setTimeout("slideShow()", interval);
+    horizontalImageChange.src = horizontalImage[i];
+    setTimeout("horizontalSlideShow()", interval);
 }
 
-document.addEventListener('DOMContentLoaded', slideShow());
+document.addEventListener('DOMContentLoaded', horizontalSlideShow());
+
+
+let j = 0;
+let verticalImage = [];
+
+
+//Create an array of the images that will cycle through on the main page
+verticalImage[0] = "slideShow/portrait/formal_3.png";
+verticalImage[1] = "slideShow/portrait/formal.png";
+verticalImage[2] = "slideShow/portrait/formal2022.JPG";
+verticalImage[3] = "slideShow/portrait/kingben.png";
+
+let verticalImageChange = document.getElementById("slideVertical");
+function verticalSlideShow(){
+    if(j < verticalImage.length - 1)
+    {
+        j++;
+    }
+    else
+    {
+        j = 0;
+    }
+    verticalImageChange.src = verticalImage[j];
+    setTimeout("verticalSlideShow()", interval);
+}
+
+document.addEventListener('DOMContentLoaded', verticalSlideShow());
 
--- members.js
+++ members.js
@@ -10,39 +10,51 @@ memberMajor[0] = "CHEM"
 //Connor Gilpatrick 
 memberImage[1] = "members/connorMember.png";
 memberDesc[1] = "Connor Gilpatrick";
+memberMajor[1] = "EVEN"
 //Matt King
 memberImage[2] = "members/mattMember.png";
 memberDesc[2] = "Matt King";
+memberMajor[2] = "ECEN"
 //Nicholas Monahan
 memberImage[3] = "members/monahanMember.png";
 memberDesc[3] = "Nicholas Monahan";
+memberMajor[3] = "MCEN"
 //Peter Arnold
 memberImage[4] = "members/peterArnold.png";
 memberDesc[4] = "Peter Arnold";
+memberMajor[4] = "MCEN"
 //Dane Robinson
 memberImage[5] = "members/daneRobinson.png";
 memberDesc[5] = "Dane Robinson";
+memberMajor[5] = "MCEN"
 //Nic Kallemeyn
 memberImage[6] = "members/nicKallemeyn.png";
 memberDesc[6] = "Nic Kallemeyn";
+memberMajor[6] = "MCEN"
 //Jacqueline Hall
 memberImage[7] = "members/jacquelineHall.png";
 memberDesc[7] = "Jacqueline Hall";
+memberMajor[7] = "EVEN"
 //Riley Dressel
 memberImage[8] = "members/rileyDressel.png";
 memberDesc[8] = "Riley Dressel";
+memberMajor[8] = "MCEN"
 //Deryus Jijina
 memberImage[8] = "members/deryusJijina.png";
 memberDesc[8] = "Deryus Jijina";
+memberMajor[8] = "CSCI"
 //Bia Santos
 memberImage[9] = "members/biaSantos.png";
 memberDesc[9] = "Bia Santos";
+memberMajor[9] = "MCEN"
 //Elias Forey
 memberImage[10] = "members/eliasForey.png";
 memberDesc[10] = "Elias Forey";
+memberMajor[10] = "ECEE"
 //Sophie Ramsey
 memberImage[11] = "members/sophieRamsey.png";
 memberDesc[11] = "Sophie Ramsey";
+memberMajor[11] = "BMEN"
 
 
 let memberPhotoClass = document.querySelectorAll(".member-photo");
@@ -62,7 +74,7 @@ function setMemberImageAndDesc() {
     }
     i = 0;
     for (const specificMajorDesc of memberMajorClass) {
-        specificMajorDesc.innerText = memberMajor[i];
+        specificMajorDesc.innerText = "Major: " + memberMajor[i];
         i++;
     }
 }
